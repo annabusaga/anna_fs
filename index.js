e68c979d94c65fcaae2a5ba1448be623e1a61344
annabusaga <anna.busaga@gmail.com>
2022-12-16 11:34:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,20 @@ console.log(typeof number);
 // console.log(Number.isNaN(validNumber)); // false
 
 
-function add(a, b, c) {
+// function add(a, b, c) {
   
-  return a-b-c;
+//   return a-b-c;
 
+// }
+
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+
+for (let i = 0; i > 10; i += 1){
+  console.log(i);
 }
 
-
-console.log(add(15, 27, 10));
-console.log(add(10, 20, 30));
-console.log(add(5, 10, 15));
+console.log(qweqwe);
