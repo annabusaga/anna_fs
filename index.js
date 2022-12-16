208548e1f31670fbde7fed0790d884c04304a280
annabusaga <anna.busaga@gmail.com>
2022-12-16 11:31:21 +0200

    commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+const age = 16;
+console.log(age);
+
+let number = 20;
+
+number = '35';
+console.log(typeof number);
+
+// const price = 200;
+// console.log("Ціна на товар:", price);
+
+// alert("Починаємо?");
+
+
+
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+// const validNumber = Number("51"); // 51
+// console.log(Number.isNaN(validNumber)); // false
+
+
+function add(a, b, c) {
+  
+  return a-b-c;
+
+}
+
+
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
